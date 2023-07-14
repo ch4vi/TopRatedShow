fcc15781a9ee51468a08fd72a057d3d93fd1b62c
Xavi Anyo <xavi.anyo@xing.com>
2023-07-14 23:35:57 +0200

    replaced groovy by kts dsl

---
+++ app/build.gradle.kts
@@ -0,0 +1,90 @@
+@file:Suppress("UnstableApiUsage")
+
+import java.io.ByteArrayOutputStream
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("kotlin-parcelize")
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
+}
+
+val appName = "Top Rated Shows"
+
+android {
+    namespace = "com.ch4vi.distilled"
+    compileSdk = rootProject.extra["compileVersion"] as Int?
+
+    defaultConfig {
+        applicationId = "com.ch4vi.distilled"
+        minSdk = rootProject.extra["minVersion"] as Int
+        targetSdk = rootProject.extra["targetVersion"] as Int
+        versionCode = 1
+        versionName = rootProject.extra["versionName"] as String
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        debug {
+            applicationIdSuffix = ".debug"
+            enableAndroidTestCoverage = true
+            enableAndroidTestCoverage = true
+            isDebuggable = true
+
+            resValue("string", "app_label", "[D] $appName")
+        }
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+
+            resValue("string", "app_label", appName)
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+    lint {
+        lintConfig = file("$rootDir/config/lint.xml")
+        checkAllWarnings = true
+        warningsAsErrors = true
+        htmlReport = true
+    }
+    testCoverage {
+        jacocoVersion = "0.8.10"
+    }
+}
+
+dependencies {
+
+    implementation(libs.bundles.common)
+    implementation(libs.bundles.ui)
+
+    kapt(libs.hilt.kapt)
+    kapt(libs.moshi.kapt)
+    kapt(libs.room.kapt)
+    implementation(libs.bundles.data)
+
+    testImplementation(libs.bundles.test)
+}
+
+kapt {
+    correctErrorTypes = true
+}
---
+++ build.gradle.kts
@@ -0,0 +1,51 @@
+@file:Suppress("DSL_SCOPE_VIOLATION")
+
+import io.gitlab.arturbosch.detekt.Detekt
+
+ext {
+    extra.apply {
+        set("versionName", "1.0.0")
+        set("versionCode", 1)
+        set("compileVersion", 33)
+        set("targetVersion", 33)
+        set("minVersion", 30)
+    }
+}
+
+plugins {
+    alias(libs.plugins.plugin.application) apply false
+    alias(libs.plugins.plugin.kotlin) apply false
+    alias(libs.plugins.plugin.detekt) apply false
+    alias(libs.plugins.plugin.hilt) apply false
+    alias(libs.plugins.plugin.ktlint) apply false
+}
+
+allprojects {
+    apply(plugin = "io.gitlab.arturbosch.detekt")
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+}
+
+subprojects {
+    tasks.withType<Detekt>().configureEach {
+        description = "Runs a custom detekt build."
+        config.setFrom(files("$rootDir/config/detekt.yml"))
+        autoCorrect = true
+        disableDefaultRuleSets = false
+
+        reports {
+            html.required.set(true)
+            html.outputLocation.set(file("build/reports/detekt.html"))
+
+            xml.required.set(false)
+            txt.required.set(false)
+            sarif.required.set(false)
+            md.required.set(false)
+        }
+        include("**/*.kt", "**/*.kts")
+        exclude("resources/", "build/")
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
